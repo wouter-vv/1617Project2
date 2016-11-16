fa7aea051b4f7cf6c5dcceea44c1d766cde6230b
Wouter Vande Velde <wouter.vandevelde@student.odisee.be>
2016-11-16 19:46:43 +0100

    Login successful!
    Signed-off-by: Wouter Vande Velde <wouter.vandevelde@student.odisee.be>

--- Code/android/src/com/odisee/project2/GoogleLoginActivity.java
+++ Code/android/src/com/odisee/project2/GoogleLoginActivity.java
@@ -2,17 +2,26 @@ package com.odisee.project2;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+
 
 public class GoogleLoginActivity extends FragmentActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
@@ -21,8 +30,12 @@ public class GoogleLoginActivity extends FragmentActivity implements GoogleApiCl
     private static final int RC_SIGN_IN = 9001;
 
     private GoogleApiClient mGoogleApiClient;
-    private TextView mStatusTextView;
+    private TextView hello;
+    private Button continueToGame;
+    private SignInButton sign_in_button;
+    private Button guest_button;
     private ProgressDialog mProgressDialog;
+    RelativeLayout.LayoutParams rlp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,14 +53,70 @@ public class GoogleLoginActivity extends FragmentActivity implements GoogleApiCl
                 .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
                 .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
                 .build();
+        sign_in_button = (SignInButton)findViewById(R.id.sign_in_button);
+        guest_button = (Button)findViewById(R.id.guest_button);
+        continueToGame = (Button)findViewById(R.id.continueToGame);
 
         findViewById(R.id.sign_in_button).setOnClickListener(this);
+        findViewById(R.id.guest_button).setOnClickListener(this);
+        findViewById(R.id.continueToGame).setOnClickListener(this);
+        hello = (TextView)findViewById(R.id.hello);
+        rlp = new RelativeLayout.LayoutParams(ActionBar.LayoutParams.WRAP_CONTENT, ActionBar.LayoutParams.WRAP_CONTENT);
     }
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            case R.id.guest_button:
+                playAsGuest();
+                break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            hello.setText("hello " + acct.getDisplayName() + "!");
+            endLogin();
+        } else {
+            hello.setText("Error logging in, try again.");
+        }
+    }
+    private void playAsGuest() {
+        hello.setText("Hello guest!");
+        endLogin();
 
     }
+
+    private void endLogin() {
+        sign_in_button.setVisibility(View.GONE);
+        continueToGame.setVisibility(View.VISIBLE);
+        guest_button.setVisibility(View.GONE);
+        rlp.setMargins(50, 0, 0, 0);
+    }
+
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
